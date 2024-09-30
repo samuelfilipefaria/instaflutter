2b6f82e2c055592d55557535ac49100961bc7fe1
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-09-30 14:02:57 -0300

    Adding account icon in top nav bar

--- lib/widgets/top_app_bar.dart
+++ lib/widgets/top_app_bar.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:instaflutter/screens/account.dart';
+import 'package:instaflutter/styles/colors.dart';
+import 'package:instaflutter/utils.dart';
 
 class TopAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const TopAppBar({super.key});
+  final bool showAccountButton;
+
+  const TopAppBar({super.key, this.showAccountButton = false});
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +17,24 @@ class TopAppBar extends StatelessWidget implements PreferredSizeWidget {
         fontWeight: FontWeight.bold,
         color: Colors.black,
         fontFamily: "LobsterTwo"
-      )
+      ),
+      actions: topBarActions(context, showAccountButton),
     );
   }
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+List<Widget> topBarActions(context, showAccountButton) {
+  if(showAccountButton) {
+    return [
+      Padding(
+        padding: const EdgeInsets.only(right: 15),
+        child: TextButton(onPressed: () { goTo(context, const Account()); }, child: const Icon(Icons.person, size: 30, color: dark)),
+      )
+    ];
+  } else {
+    return [];
+  }
 }
