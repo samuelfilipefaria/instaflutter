344390f981386a890ffa51dafd5cf4aa13f7d5e1
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-09-30 14:04:07 -0300

    Moving utils methods to one file

--- lib/utils.dart
+++ lib/utils.dart
@@ -3,3 +3,10 @@ import 'package:flutter/material.dart';
 void showSnackBar(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
 }
+
+void goTo(BuildContext context, Widget route) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => route),
+  );
+}
