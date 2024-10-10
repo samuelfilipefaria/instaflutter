d058981d8c06484abcd72a3f5fe3be4d5626ded2
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-10-09 22:02:27 -0300

    Improving post widget

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:instaflutter/styles/colors.dart';
+import 'package:instaflutter/utils.dart';
+import 'package:instaflutter/widgets/action_button.dart';
 
 class Post extends StatelessWidget {
   final String user;
@@ -15,28 +18,39 @@ class Post extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        border: Border.all(color: dark, style: BorderStyle.solid, width: 1)
-      ),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 10, bottom: 20, top: 20),
-            child: SizedBox(width: 350, child: Row(children: [const  Icon(Icons.person), Text(user)]))
-          ),
-          Image.network(
-            width: 350,
-            height: 350,
-            photo
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10, bottom: 20, top: 20),
-            child: SizedBox(width: 350, child: Text(caption))
-          ),
-        ],
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 10),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: dark, style: BorderStyle.solid, width: 1)
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10, bottom: 20, top: 20),
+              child: SizedBox(width: 350, child: Row(children: [const  Icon(Icons.person), Text(user)]))
+            ),
+            Image.network(
+              width: 350,
+              height: 350,
+              photo
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, bottom: 20, top: 20),
+              child: SizedBox(width: 350, child: Text(caption))
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, bottom: 20, top: 20),
+              child: ActionButton(label: "Copy photo link", action: () => copyPhotoUrl(photo, context))
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void copyPhotoUrl(url, context) {
+    Clipboard.setData(ClipboardData(text: photo));
+    showSnackBar(context, "Photo url copied to clipboard!");
   }
 }
